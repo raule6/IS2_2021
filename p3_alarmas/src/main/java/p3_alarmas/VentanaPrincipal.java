465126a5ba97838d6122154fb3aebbc39c8d60b6
pag804 <pag804@alumnos.unican.es>
2021-04-04 14:29:33 +0200

    04/04/2021

--- p3_alarmas/src/main/java/p3_alarmas/VentanaPrincipal.java
+++ p3_alarmas/src/main/java/p3_alarmas/VentanaPrincipal.java
@@ -27,24 +27,29 @@ import java.util.Calendar;
 import java.awt.Color;
 import javax.swing.JTextPane;
 
-//TODO: prueba sonido
-import java.applet.AudioClip;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class VentanaPrincipal {
 
 	private JFrame frmAlarma;
 	private JTextField idAlarma;
-	
+
 	private Alarmas alarmasController;
-	
+
 	private JList<String> listActive, listInactive;
 	private JLabel lblMsgOffOn, lblMsgAddAlarm;
 	private JTextPane txtAlert;
-	
+	private JSpinner horaAlarma;
+	private JButton btnNuevaAlarma, btnEliminar, btnOff, btnOn;
+
 	private Timer timer = new Timer();
 	private MsgClearTask task = new MsgClearTask(this);
-	
-	private AudioClip snd = java.applet.Applet.newAudioClip(getClass().getResource("/p3_alarmas/ClockAlarmSound.wav"));
+
+	private Clip snd;
 
 	/**
 	 * Launch the application.
@@ -76,31 +81,31 @@ public class VentanaPrincipal {
 	private void initialize() {
 		
 		alarmasController = new Alarmas(this);
-		
+
 		frmAlarma = new JFrame();
 		frmAlarma.setResizable(false);
-		frmAlarma.setTitle("Alarma");
+		frmAlarma.setTitle("Alarma - by Pablo & Raúl");
 		frmAlarma.setBounds(100, 100, 470, 440);
 		frmAlarma.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAlarma.getContentPane().setLayout(null);
-		
+
 		JLabel lbl_idAlarma = new JLabel("ID Alarma");
 		lbl_idAlarma.setFont(new Font("Roboto", Font.PLAIN, 15));
 		lbl_idAlarma.setBounds(21, 51, 82, 13);
 		frmAlarma.getContentPane().add(lbl_idAlarma);
-		
+
 		idAlarma = new JTextField();
 		idAlarma.setFont(new Font("Roboto", Font.PLAIN, 12));
 		idAlarma.setBounds(117, 50, 96, 19);
 		frmAlarma.getContentPane().add(idAlarma);
 		idAlarma.setColumns(10);
-		
+
 		JLabel lbl_horaAlarma = new JLabel("Hora Alarma");
 		lbl_horaAlarma.setFont(new Font("Roboto", Font.PLAIN, 15));
 		lbl_horaAlarma.setBounds(21, 79, 96, 13);
 		frmAlarma.getContentPane().add(lbl_horaAlarma);
-		
-		JSpinner horaAlarma = new JSpinner();
+
+		horaAlarma = new JSpinner();
 		Date dActual = new Date();
 		Calendar cActual = Calendar.getInstance();
 		dActual = cActual.getTime();
@@ -110,61 +115,32 @@ public class VentanaPrincipal {
 		JSpinner.DateEditor de = new JSpinner.DateEditor(horaAlarma, "HH:mm");
 		horaAlarma.setEditor(de);
 		frmAlarma.getContentPane().add(horaAlarma);
-		
+
 		lblMsgAddAlarm = new JLabel("");
 		lblMsgAddAlarm.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMsgAddAlarm.setFont(new Font("Roboto", Font.PLAIN, 12));
 		lblMsgAddAlarm.setBounds(21, 144, 192, 40);
 		frmAlarma.getContentPane().add(lblMsgAddAlarm);
-		
-		JButton btnNuevaAlarma = new JButton("Nueva Alarma");
+
+		btnNuevaAlarma = new JButton("Nueva Alarma");
 		btnNuevaAlarma.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
-				if (idAlarma.getText().isBlank() || alarmasController.buscaAlarmaActiva(idAlarma.getText()) != null || alarmasController.buscaAlarmaDesactivada(idAlarma.getText()) != null) {
-					setMsgAddAlarm("¡Debes introducir una ID válida!", true);
-					return;
+				if (btnNuevaAlarma.isEnabled()) {
+					if (idAlarma.getText().isBlank() || alarmasController.buscaAlarmaActiva(idAlarma.getText()) != null || alarmasController.buscaAlarmaDesactivada(idAlarma.getText()) != null) {
+						setMsgAddAlarm("¡Debes introducir una ID válida!", true);
+						return;
+					}
+
+					alarmasController.NuevaAlarma(idAlarma.getText(), setTime((Date) horaAlarma.getValue()));
+					updateLists();
 				}
-				
-				Date date = (Date) horaAlarma.getValue();
-				/*Date date = (Date) horaAlarma.getValue();
-				Calendar c = Calendar.getInstance();
-				c.setTime(date);
-				int h = c.get(Calendar.HOUR);
-				int min = c.get(Calendar.MINUTE);
-				
-				// Fecha actual.
-				Date d = new Date();
-				c = Calendar.getInstance();
-				c.setTime(d);
-				int ah = c.get(Calendar.HOUR);
-				int amin = c.get(Calendar.MINUTE);
-				
-				Date when = new Date();
-				c = Calendar.getInstance();
-				c.setTime(when);
-				c.set(Calendar.HOUR, h);
-				c.set(Calendar.MINUTE, min);
-				c.set(Calendar.SECOND, 0);
-				
-				if (60 * ah + amin > 60 * h + min) {
-					c.set(Calendar.DAY_OF_YEAR, c.get(Calendar.DAY_OF_YEAR) + 1);
-					setMsgAddAlarm("La alarma sonará mañana", false);
-				} else {
-					setMsgAddAlarm("La alarma sonará hoy", false);
-				}
-				
-				when = c.getTime();*/
-				
-				alarmasController.NuevaAlarma(idAlarma.getText(), setTime(date));
-				updateLists();
 			}
 		});
 		btnNuevaAlarma.setFont(new Font("Roboto", Font.BOLD, 15));
 		btnNuevaAlarma.setBounds(21, 115, 192, 21);
 		frmAlarma.getContentPane().add(btnNuevaAlarma);
-		
+
 		JButton btnApagar = new JButton("\u00A1APAGAR!");
 		btnApagar.addMouseListener(new MouseAdapter() {
 			@Override
@@ -173,58 +149,61 @@ public class VentanaPrincipal {
 				updateLists();
 			}
 		});
-		
+
 		btnApagar.setFont(new Font("Roboto", Font.BOLD, 18));
 		btnApagar.setBounds(50, 332, 129, 48);
 		frmAlarma.getContentPane().add(btnApagar);
-		
+
 		JLabel lbl_AlarmasActivas = new JLabel("Alarmas activas");
 		lbl_AlarmasActivas.setFont(new Font("Roboto", Font.PLAIN, 15));
 		lbl_AlarmasActivas.setBounds(283, 22, 110, 13);
 		frmAlarma.getContentPane().add(lbl_AlarmasActivas);
-		
+
 		JLabel lbl_AlarmasDesactivadas = new JLabel("Alarmas desactivadas");
 		lbl_AlarmasDesactivadas.setFont(new Font("Roboto", Font.PLAIN, 15));
 		lbl_AlarmasDesactivadas.setBounds(259, 167, 155, 17);
 		frmAlarma.getContentPane().add(lbl_AlarmasDesactivadas);
-		
-		JButton btnNewButton = new JButton("ELIMINAR");
-		btnNewButton.addMouseListener(new MouseAdapter() {
+
+		btnEliminar = new JButton("ELIMINAR");
+		btnEliminar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				int i = listActive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
-				int o = listInactive.getSelectedIndex();
-				if (i != -1) {
-					Alarma a = (Alarma) alarmasController.alarmasActivas().toArray()[i];
-					alarmasController.BorraAlarma(a.getID());
+				if (btnEliminar.isEnabled()) {
+					//TODO: no funciona bien si en desactivadas hay muchas (cuando aparece el scroll)
+					int i = listActive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
+					int o = listInactive.getSelectedIndex();
+					if (i != -1) {
+						Alarma a = (Alarma) alarmasController.alarmasActivas().toArray()[i];
+						alarmasController.BorraAlarma(a.getID());
+					}
+					if (o == -1 && i == -1) {
+						setMsgOffOn("Selecciona una alarma");
+					} else if (o != -1) {
+						Alarma a = (Alarma) alarmasController.alarmasDesactivadas().get(o);
+						alarmasController.BorraAlarma(a.getID());
+					}
+					updateLists();
 				}
-				if (o == -1 && i == -1) {
-					setMsgOffOn("Selecciona una alarma");
-				} else if (o != -1) {
-					Alarma a = (Alarma) alarmasController.alarmasDesactivadas().get(o);
-					alarmasController.BorraAlarma(a.getID());
-				}
-				updateLists();
 			}
 		});
-		btnNewButton.setFont(new Font("Roboto", Font.BOLD, 16));
-		btnNewButton.setBounds(283, 346, 111, 34);
-		frmAlarma.getContentPane().add(btnNewButton);
-		
+		btnEliminar.setFont(new Font("Roboto", Font.BOLD, 16));
+		btnEliminar.setBounds(283, 346, 111, 34);
+		frmAlarma.getContentPane().add(btnEliminar);
+
 		lblMsgOffOn = new JLabel("");
 		lblMsgOffOn.setForeground(Color.RED);
 		lblMsgOffOn.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMsgOffOn.setFont(new Font("Roboto", Font.PLAIN, 12));
 		lblMsgOffOn.setBounds(229, 296, 227, 19);
 		frmAlarma.getContentPane().add(lblMsgOffOn);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(259, 41, 155, 116);
 		frmAlarma.getContentPane().add(scrollPane);
-		
+
 		listActive = new JList();
 		listActive.addMouseListener(new MouseAdapter() {
-		@Override
+			@Override
 			public void mouseClicked(MouseEvent e) {
 				listInactive.clearSelection();
 			}
@@ -240,11 +219,11 @@ public class VentanaPrincipal {
 		});
 		scrollPane.setViewportView(listActive);
 		listActive.setFont(new Font("Roboto", Font.PLAIN, 13));
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(259, 188, 155, 110);
 		frmAlarma.getContentPane().add(scrollPane_1);
-		
+
 		listInactive = new JList();
 		listInactive.addMouseListener(new MouseAdapter() {
 			@Override
@@ -264,60 +243,63 @@ public class VentanaPrincipal {
 		});
 		scrollPane_1.setViewportView(listInactive);
 		listActive.setFont(new Font("Roboto", Font.PLAIN, 13));
-		
-		JButton btnOff = new JButton("OFF");
+
+		btnOff = new JButton("OFF");
 		btnOff.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				int i = listActive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
-				
-				if (i == -1) {
-					setMsgOffOn("Selecciona una alarma activa");
-				} else {
-					setMsgOffOn("");
-					Alarma a = (Alarma) alarmasController.alarmasActivas().toArray()[i];
-					alarmasController.AlarmaOff(a.getID());
+				if (btnOff.isEnabled()) {
+					int i = listActive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
+					if (i == -1) {
+						setMsgOffOn("Selecciona una alarma activa");
+					} else {
+						setMsgOffOn("");
+						Alarma a = (Alarma) alarmasController.alarmasActivas().toArray()[i];
+						alarmasController.AlarmaOff(a.getID());
+					}
+					updateLists();
 				}
-				updateLists();
 			}
 		});
 		btnOff.setFont(new Font("Roboto", Font.PLAIN, 13));
 		btnOff.setBounds(249, 315, 85, 21);
 		frmAlarma.getContentPane().add(btnOff);
-		
-		JButton btnOn = new JButton("ON");
+
+		btnOn = new JButton("ON");
 		btnOn.setFont(new Font("Roboto", Font.PLAIN, 13));
 		btnOn.setBounds(344, 315, 85, 21);
 		btnOn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				int i = listInactive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
-				
-				Alarma a = (Alarma) alarmasController.alarmasDesactivadas().get(i);
-				
-				if (i == -1) {
-					setMsgOffOn("Selecciona una alarma desactivada");
-				} else {
-					setMsgOffOn("");
+				if (btnOn.isEnabled()) {
+					int i = listInactive.getSelectedIndex(); // -1 si no hay no hay nada seleccionado
 
-					Date date = a.getHora();
-					
-					a.setHora(setTime(date));
-					
-					alarmasController.AlarmaOn(a.getID());
+					Alarma a = (Alarma) alarmasController.alarmasDesactivadas().get(i);
+					if (i == -1) {
+						setMsgOffOn("Selecciona una alarma desactivada");
+					} else {
+						setMsgOffOn("");
+
+						Date date = a.getHora();
+
+						a.setHora(setTime(date));
+
+						alarmasController.AlarmaOn(a.getID());
+					}
+					updateLists();
 				}
-				updateLists();
+
 			}
 		});
 		frmAlarma.getContentPane().add(btnOn);
-		
+
 		txtAlert = new JTextPane();
 		txtAlert.setEditable(false);
 		txtAlert.setFont(new Font("Roboto", Font.PLAIN, 20));
 		txtAlert.setBounds(21, 227, 192, 71);
 		frmAlarma.getContentPane().add(txtAlert);
 	}
-	
+
 	/**
 	 * Método auxiliar que actualiza las listas de alarmas
 	 */
@@ -330,7 +312,7 @@ public class VentanaPrincipal {
 		}
 		listActive.setModel(modeloActivo);
 		listActive.updateUI();
-		
+
 		// Update desactivadas
 		DefaultListModel<String> modeloInactivo = new DefaultListModel<String>();
 		List<Alarma> desactivadas = alarmasController.alarmasDesactivadas();
@@ -340,34 +322,34 @@ public class VentanaPrincipal {
 		listInactive.setModel(modeloInactivo);
 		listInactive.updateUI();
 	}
-	
+
 	private Date setTime(Date date) {
 		Calendar c = Calendar.getInstance();
 		c.setTime(date);
 		int h = c.get(Calendar.HOUR);
 		int min = c.get(Calendar.MINUTE);
-		
+
 		// Fecha actual.
 		Date d = new Date();
 		c = Calendar.getInstance();
 		c.setTime(d);
 		int ah = c.get(Calendar.HOUR);
 		int amin = c.get(Calendar.MINUTE);
-		
+
 		Date when = new Date();
 		c = Calendar.getInstance();
 		c.setTime(when);
 		c.set(Calendar.HOUR, h);
 		c.set(Calendar.MINUTE, min);
 		c.set(Calendar.SECOND, 0);
-		
+
 		if (60 * ah + amin >= 60 * h + min) {
 			c.set(Calendar.DAY_OF_YEAR, c.get(Calendar.DAY_OF_YEAR) + 1);
 			setMsgAddAlarm("La alarma sonará mañana", false);
 		} else {
 			setMsgAddAlarm("La alarma sonará hoy", false);
 		}
-		
+
 		when = c.getTime();
 		return when;
 	}
@@ -384,30 +366,49 @@ public class VentanaPrincipal {
 		timer.schedule(task, 5000);
 	}
 	
-	
 	private void setMsgOffOn(String s) {
 		lblMsgOffOn.setText(s);
 	}
-	
+
 	public void suenaAlarma() {
-		txtAlert.setText("ESTÁ SONANDO UNA ALARMA!");
-		snd.play();
+		setEnabledMenuOptions(false);
+		txtAlert.setText("ESTÁ SONANDO LA ALARMA " + alarmasController.alarmaMasProxima().getID());
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\pablo\\Desktop\\CARRERA\\3º de carrera\\2o cuatrimestre\\Software II\\practica3\\IS2_2021\\workspace-eclipse-IS2_2021\\p3_alarmas\\src\\main\\java\\p3_alarmas\\alarmSnd.wav").getAbsoluteFile());
+			snd = AudioSystem.getClip();
+			snd.open(audioInputStream);
+			snd.loop(Clip.LOOP_CONTINUOUSLY);
+			snd.start();
+		} catch(Exception ex) {
+			System.out.println("Error al reproducir el sonido");
+			ex.printStackTrace();
+		}
 	}
-	
+
 	public void apagaAlarma() {
+		setEnabledMenuOptions(true);
 		txtAlert.setText("");
 		snd.stop();
-		snd = java.applet.Applet.newAudioClip(getClass().getResource("/p3_alarmas/ClockAlarmSound.wav"));
+	}
+
+	private void setEnabledMenuOptions(boolean opt) {
+		idAlarma.setEnabled(opt);
+		horaAlarma.setEnabled(opt);
+		btnNuevaAlarma.setEnabled(opt);
+		btnEliminar.setEnabled(opt);
+		btnOff.setEnabled(opt);
+		btnOn.setEnabled(opt);
 	}
 	
 	public class MsgClearTask extends TimerTask {
-		 private VentanaPrincipal context;
-		 public MsgClearTask (VentanaPrincipal c) {
-			 context = c;
-		 }
-		 // Tarea que se ejecuta cuando se alcanza el tiempo
-		 public void run() {
-			 context.setMsgAddAlarm("", false);
-		 }
+		private VentanaPrincipal context;
+		public MsgClearTask (VentanaPrincipal c) {
+			context = c;
+		}
+		// Tarea que se ejecuta cuando se alcanza el tiempo
+		public void run() {
+			context.setMsgAddAlarm("", false);
+		}
 	}
+
 }
